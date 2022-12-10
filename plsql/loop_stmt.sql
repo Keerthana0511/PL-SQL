a3b1b07bdc862cb5bdd5d72b30b8776cbd59c6ad
Keerthana D <100903799+Keerthana0511@users.noreply.github.com>
2022-12-10 13:27:51 +0530

    Update loop_stmt.sql

--- plsql/loop_stmt.sql
+++ plsql/loop_stmt.sql
@@ -3,11 +3,13 @@ DECLARE
 v_counter NUMBER:=0;
 v_result NUMBER;
 BEGIN
-WHILE v_counter<=10 LOOP
- 
+ LOOP
+ v_counter :=v_counter+1;
  v_result:=9*v_counter;
  DBMS_OUTPUT.PUT_LINE('9'||'x'||v_counter||'='||v_result);
- v_counter :=v_counter+1;
+ IF v_counter>=10 THEN
+  EXIT;
+ END IF;
 END LOOP;
 DBMS_OUTPUT.PUT_LINE('Thank you');
 END;
